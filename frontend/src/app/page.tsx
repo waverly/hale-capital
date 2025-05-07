a284baf1e798afdc382cc004841210bccdb4a468
Eric Jacobsen <eric@whiskyvangoghgo.com>
2025-05-06 22:05:43 -0400

    sanity rebuild

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,18 +2,17 @@ import { processMetadata } from "@lib"
 // import { Page } from "@ui"
 import { BASE_URL, DEFAULT_SITE_TITLE } from "@const"
 import { getPage, getMetadata, getSiteSettings } from "@query"
-import { SLUG } from "./"
 import { notFound } from "next/navigation"
 import { Home } from "@ui"
 
 export default async function HomePage() {
-  const pageData = await getPage({ slug: SLUG })
+  const pageData = await getPage({ slug: "home" })
   if (!pageData) return notFound()
   return <Home pageData={pageData} />
 }
 
 export async function generateMetadata() {
-  const metadataResult = await getMetadata({ slug: SLUG })
+  const metadataResult = await getMetadata({ slug: "home" })
   const siteSettings = await getSiteSettings()
   const metadata = processMetadata(metadataResult, siteSettings, "page")
   return {
